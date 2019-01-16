12c94a64ed921e8dd00b6cac59f2d3f5fe26331b
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-01-16 10:05:36 -0600

    completed 9-13

--- chinook.sql
+++ chinook.sql
@@ -45,6 +45,44 @@ ORDER BY Employee.LastName;
 
 -- 8 total_invoices_{year}.sql: How many Invoices were there in 2009 and 2011?
 -- SELECT * FROM TableName where DATEPART(yy, YourDateColumn) = 1996 --For Year
+-- SELECT COUNT() as "total Invoices 2009 and 2011"
+-- FROM Invoice
+-- WHERE strftime('%Y', Invoice.invoiceDate) = "2009" OR  strftime('%Y', Invoice.invoiceDate) = "2011";
+
 SELECT COUNT() as "total Invoices 2009 and 2011"
 FROM Invoice
-WHERE strftime('%Y', Invoice.invoiceDate) = "2009" OR  strftime('%Y', Invoice.invoiceDate) = "2011";
+WHERE Invoice.InvoiceDate LIKE "2011%"
+OR Invoice.InvoiceDate LIKE "2009%";
+
+-- 9 - total_sales_{year}.sql: What are the respective total sales for each of those years? Can only get one year at a time
+SELECT strftime('%Y', Invoice.invoiceDate) as "Year" , SUM(Invoice.Total) as "Total Sales"
+FROM Invoice
+WHERE Invoice.InvoiceDate LIKE "2011%";
+
+-- 10 invoice_37_line_item_count.sql: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+SELECT COUNT(InvoiceLine.InvoiceId) as "Number of Line Items on Invoice"
+FROM InvoiceLine
+WHERE InvoiceLine.InvoiceId = 37;
+
+-- 11 line_items_per_invoice.sql: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+SELECT InvoiceLine.InvoiceId, COUNT(InvoiceLine.InvoiceId) as "Number of Line Items on Invoice"
+FROM InvoiceLine
+GROUP BY InvoiceLine.InvoiceId;
+
+-- 12 line_item_track.sql: Provide a query that includes the purchased track name with each invoice line item.
+SELECT InvoiceLine.InvoiceLineId as "Invoice Line Item", Track.Name
+FROM InvoiceLine
+JOIN Track
+ON InvoiceLine.TrackId = Track.TrackId
+ORDER BY InvoiceLine.InvoiceLineId;
+
+-- 13 line_item_track_artist.sql: Provide a query that includes the purchased track name AND artist name with each invoice line item.
+SELECT InvoiceLine.InvoiceLineId as "Invoice Line Item", Track.Name as "Track Name", Artist.Name
+FROM InvoiceLine
+JOIN Track ON InvoiceLine.TrackId = Track.TrackId
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist ON Album.ArtistId = Artist.ArtistId;
+
+
+
+
